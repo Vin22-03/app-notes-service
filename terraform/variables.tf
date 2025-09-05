866faa56427bf248af05797d6aeec19d8d29df5e
Vin22-03 <vinay.venvin@gmail.com>
2025-09-05 09:33:59 +0000

    updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,10 @@ variable "image_tag" {
   description = "Tag of the Docker image to use"
   type        = string
 }
+
+
+# variables.tf
+variable "ecs_cluster" {
+  description = "Name of the ECS Cluster"
+  type        = string
+}
