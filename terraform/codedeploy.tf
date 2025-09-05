c2e003302e16be7aabcb97ce3eb0d4beab59ef07
Vin22-03 <vinay.venvin@gmail.com>
2025-09-05 09:10:11 +0000

    updated

--- terraform/codedeploy.tf
+++ terraform/codedeploy.tf
@@ -1,38 +1,24 @@
+# codedeploy.tf
 resource "aws_codedeploy_app" "ecs_app" {
   name = "vin-notes-codedeploy"
   compute_platform = "ECS"
 }
 
-resource "aws_iam_role" "codedeploy_role" {
-  name = "vin-codedeploy-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "codedeploy.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "codedeploy_attach" {
-  role       = aws_iam_role.codedeploy_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRoleForECS"
-}
-
 resource "aws_codedeploy_deployment_group" "ecs_dg" {
   app_name              = aws_codedeploy_app.ecs_app.name
-  deployment_group_name = "vin-notes-deploy-group"
+  deployment_group_name = "vin-notes-dg"
   service_role_arn      = aws_iam_role.codedeploy_role.arn
+
   deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
 
   blue_green_deployment_config {
     terminate_blue_instances_on_deployment_success {
-      action = "TERMINATE"
+      action                          = "TERMINATE"
       termination_wait_time_in_minutes = 5
     }
 
@@ -43,28 +29,22 @@ resource "aws_codedeploy_deployment_group" "ecs_dg" {
   }
 
   ecs_service {
-    cluster_name = aws_ecs_cluster.main.name
+    cluster_name = var.ecs_cluster
     service_name = aws_ecs_service.notes_service_v4.name
   }
 
   load_balancer_info {
     target_group_pair_info {
-      prod_traffic_route {
-        listener_arns = [aws_lb_listener.notes_listener_v4.arn]
-      }
-
       target_group {
         name = aws_lb_target_group.notes_tg_v4.name
       }
-
       target_group {
         name = aws_lb_target_group.notes_tg_v4_green.name
       }
+
+      prod_traffic_route {
+        listener_arns = [aws_lb_listener.notes_listener_v4.arn]
+      }
     }
   }
-
-  depends_on = [
-    aws_lb_listener.notes_listener_v4,
-    aws_ecs_service.notes_service_v4
-  ]
 }
